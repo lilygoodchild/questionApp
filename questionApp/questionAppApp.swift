7a5ee5d8398009fe1b251de65492eabc59db5b85
Lily <lsmgoodchild@icloud.com>
2023-07-28 10:57:54 +0100

    Initial Commit

---
+++ questionApp/questionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  questionAppApp.swift
+//  questionApp
+//
+//  Created by scholar on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct questionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
